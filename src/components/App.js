8e708c2a27ca94ce2322051eedaf90ca5cbe005a
salman3009 <salman444g@gmail.com>
2022-10-20 19:10:13 +0530

    code

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,14 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+
+      <div>
+        <input type="number" name="work-duration" data-testid='work-duration'/>
+        <input type="number" name="work-duration" data-testid='work-duration'/>
+
+      </div>
+    </div>
   )
 }
 
